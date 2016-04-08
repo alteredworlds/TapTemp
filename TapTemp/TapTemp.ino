cac79ef5e8cb5561c36eedc98ab5f4941d32724a
Tom Gilbert <tom@alteredworlds.com>
2016-04-08 17:26:17 +0100

    Support retrieval from a specific time on given day

--- TapTemp/TapTemp.ino
+++ TapTemp/TapTemp.ino
@@ -450,9 +450,12 @@ boolean handleBleCommands() {
                     
                     // iterate over every line
                     while (readRecord(&unixTimestamp, sample)) {
-                        // send valid record via existing ble mechanism
-                        bleWriteData(unixTimestamp, sample);
-                        ble_do_events();
+                        // we only want to send data at or after requested timestamp
+                        if (unixTimestamp >= date.unixTime) {
+                            // send valid record via existing ble mechanism
+                            bleWriteData(unixTimestamp, sample);
+                            ble_do_events();
+                        }
                     }
                     
                     // close the file
